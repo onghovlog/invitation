b4150e959a045e6b8ea4853765793c338cd7fe72
Thầy Hộ Fpoly <54646892+onghovlog@users.noreply.github.com>
2023-09-20 08:48:09 +0700

    add 7 thumoi lan 3
    thêm 7 thumoi lan 3

--- assets/js/play.js
+++ assets/js/play.js
@@ -1,41 +1,48 @@
 let allLoaded = false;
 let countLoaded = 0;
 let checkLoaded = () => {
-  countLoaded++;
-  if (countLoaded === 2) {
-    allLoaded = true;
-    clearID = setInterval(updateLogger, 950);
-  }
+    countLoaded++;
+    if (countLoaded === 2) {
+        allLoaded = true;
+        clearID = setInterval(updateLogger, 950);
+    }
 };
 
 // Image Array
 let letterArray = [
-  "thu_moi_01_thay_Vu_Chi_Thanh.jpg",
-  "thu_moi_02_thay_Hoang_Van_Loi.jpg",
-  "thu_moi_03_thay_Tran_Van_Nam.jpg",
-  "thu_moi_04_Co_Nguyen_Thi_Kim_Phuong.jpg",
-  "thu_moi_05_thay_Nguyen_Thanh_Nam.jpg",
-  "thu_moi_06_thay_Dinh_An.jpg",
-  "thu_moi_07_thay_Tran_Ly_Anh_Tuan.jpg",
-  "thu_moi_08_Thay_Le_Quoc_Nam.jpg",
-  "thu_moi_09_co_Thu_Hang.jpg",
-  "thu_moi_10_Thay_Tran_Minh_Hung.jpg",
-  "thu_moi_11_Thay_Nguyen_Khanh.jpg",
-  "thu_moi_12_thay_Phuoc_Cuong.jpg",
-  "thu_moi_13_Co_Tran_Thi_Hoang_Phuong.jpg",
-  "thu_moi_14_Thay_Pham_Tuan_Anh.jpg",
-  "thu_moi_15_Thay_Nguyen-Duc_Anh_Tuan.jpg",
-  "thu_moi_16_Co_Vu_Thanh_Huyen_DN.jpg",
-  "thu_moi_17_Thay_Huynh_Thanh_Hoang_CT.jpg",
-  "thu_moi_18_Thay_Tran_Quang_Sang.jpg",
-  "thu_moi_19_thay_Tran_Duy_Phong.jpg",
-  "thu_moi_20_Co_Tran_Thi_Huong.jpg",
-  "thu_moi_21_Thay_Than_Hoang_Loc.jpg",
-  "thu_moi_23_Thay_Hoang_Anh.jpg",
-  "thu_moi_23_Thay_HoangHuy.jpg",
-  "thu_moi_DN_Anh_ChristianNguyen.jpg",
-  "thu_moi_DN_Anh_HoangNam.jpg",
-  "thu_moi_DN_Anh_Nhu_Y.jpg"
+    "thu_moi_01_thay_Vu_Chi_Thanh.jpg",
+    "thu_moi_02_thay_Hoang_Van_Loi.jpg",
+    "thu_moi_03_thay_Tran_Van_Nam.jpg",
+    "thu_moi_04_Co_Nguyen_Thi_Kim_Phuong.jpg",
+    "thu_moi_05_thay_Nguyen_Thanh_Nam.jpg",
+    "thu_moi_06_thay_Dinh_An.jpg",
+    "thu_moi_07_thay_Tran_Ly_Anh_Tuan.jpg",
+    "thu_moi_08_Thay_Le_Quoc_Nam.jpg",
+    "thu_moi_09_co_Thu_Hang.jpg",
+    "thu_moi_10_Thay_Tran_Minh_Hung.jpg",
+    "thu_moi_11_Thay_Nguyen_Khanh.jpg",
+    "thu_moi_12_thay_Phuoc_Cuong.jpg",
+    "thu_moi_13_Co_Tran_Thi_Hoang_Phuong.jpg",
+    "thu_moi_14_Thay_Pham_Tuan_Anh.jpg",
+    "thu_moi_15_Thay_Nguyen-Duc_Anh_Tuan.jpg",
+    "thu_moi_16_Co_Vu_Thanh_Huyen_DN.jpg",
+    "thu_moi_17_Thay_Huynh_Thanh_Hoang_CT.jpg",
+    "thu_moi_18_Thay_Tran_Quang_Sang.jpg",
+    "thu_moi_19_thay_Tran_Duy_Phong.jpg",
+    "thu_moi_20_Co_Tran_Thi_Huong.jpg",
+    "thu_moi_21_Thay_Than_Hoang_Loc.jpg",
+    "thu_moi_23_Thay_Hoang_Anh.jpg",
+    "thu_moi_23_Thay_HoangHuy.jpg",
+    "thu_moi_DN_Anh_ChristianNguyen.jpg",
+    "thu_moi_DN_Anh_HoangNam.jpg",
+    "thu_moi_DN_Anh_Nhu_Y.jpg",
+    "thu_moi_27_HuynhThanh.jpg",
+    "thu_moi_28_QuyCBNV.jpg",
+    "thu_moi_29_QuynhNhu.jpg",
+    "thu_moi_30_QuySinhVien.jpg",
+    "thu_moi_31_QuyThayCo.jpg",
+    "thu_moi_32_TrungVo.jpg",
+    "thu_moi_33_YVan.jpg"
 ];
 
 let currentURL = window.location.href;
@@ -65,85 +72,85 @@ let letterScore;
 let resetGame;
 
 function letterAction() {
-  // Lưu trữ các hàm gắn với sự kiện để loại bỏ sau này
-  let letterOpenClickHandler;
-  let letterCloseClickHandler;
+    // Lưu trữ các hàm gắn với sự kiện để loại bỏ sau này
+    let letterOpenClickHandler;
+    let letterCloseClickHandler;
 
-  // Get title
-  letterTitle = document.querySelector(".letter-title");
-  // Show Letter
-  markContainer.style.display = "none";
-  letterContainer = document.querySelector(".letter");
-  if (gift.isPassed) {
-    letterTitle.innerHTML = "CHIẾN THẮNG";
-    letterContainer.classList.add("full");
-  } else {
-    letterTitle.innerHTML = "GAME OVER";
-    letterContainer.classList.add("nogift");
-  }
-  letterScore = document.querySelector("#score");
-  letterScore.innerHTML = score;
-  // Show Content
-  if (gift.isPassed) {
-    letterOpenBtn = document.querySelector(".letter-open");
+    // Get title
+    letterTitle = document.querySelector(".letter-title");
+    // Show Letter
+    markContainer.style.display = "none";
+    letterContainer = document.querySelector(".letter");
+    if (gift.isPassed) {
+        letterTitle.innerHTML = "CHIẾN THẮNG";
+        letterContainer.classList.add("full");
+    } else {
+        letterTitle.innerHTML = "GAME OVER";
+        letterContainer.classList.add("nogift");
+    }
+    letterScore = document.querySelector("#score");
+    letterScore.innerHTML = score;
+    // Show Content
+    if (gift.isPassed) {
+        letterOpenBtn = document.querySelector(".letter-open");
 
-    // Lưu hàm gắn với sự kiện click để loại bỏ sau này
-    letterOpenClickHandler = () => {
-      letterContent = document.querySelector(".leter-content");
-      letterContent.classList.add("active");
-      // Ẩn thư
-      letterClose = document.getElementsByClassName("letter-close");
-      letterClose = Array.from(letterClose);
-      letterClose.forEach((element) => {
         // Lưu hàm gắn với sự kiện click để loại bỏ sau này
-        letterCloseClickHandler = () => {
-          letterContent.classList.remove("active");
+        letterOpenClickHandler = () => {
+            letterContent = document.querySelector(".leter-content");
+            letterContent.classList.add("active");
+            // Ẩn thư
+            letterClose = document.getElementsByClassName("letter-close");
+            letterClose = Array.from(letterClose);
+            letterClose.forEach((element) => {
+                // Lưu hàm gắn với sự kiện click để loại bỏ sau này
+                letterCloseClickHandler = () => {
+                    letterContent.classList.remove("active");
+                };
+                element.addEventListener("click", letterCloseClickHandler);
+            });
         };
-        element.addEventListener("click", letterCloseClickHandler);
-      });
-    };
 
-    letterOpenBtn.addEventListener("click", letterOpenClickHandler);
-  }
-
-  // Reset Game
-  resetGame = document.querySelector(".reset-game");
-  resetGame.addEventListener("click", () => {
-    // Loại bỏ sự kiện click đã đăng ký khi nút "Reset Game" được click
-    if (letterOpenClickHandler) {
-      letterOpenBtn.removeEventListener("click", letterOpenClickHandler);
-    }
-    if (letterCloseClickHandler) {
-      letterClose.forEach((element) => {
-        element.removeEventListener("click", letterCloseClickHandler);
-      });
+        letterOpenBtn.addEventListener("click", letterOpenClickHandler);
     }
 
-    document.querySelector(
-      ".letter-btn"
-    ).innerHTML = `<p class="letter-loading">Vui lòng chờ...</p>`;
-    setTimeout(() => {
-      window.location.reload();
-    }, 1000);
-  });
+    // Reset Game
+    resetGame = document.querySelector(".reset-game");
+    resetGame.addEventListener("click", () => {
+        // Loại bỏ sự kiện click đã đăng ký khi nút "Reset Game" được click
+        if (letterOpenClickHandler) {
+            letterOpenBtn.removeEventListener("click", letterOpenClickHandler);
+        }
+        if (letterCloseClickHandler) {
+            letterClose.forEach((element) => {
+                element.removeEventListener("click", letterCloseClickHandler);
+            });
+        }
+
+        document.querySelector(
+            ".letter-btn"
+        ).innerHTML = `<p class="letter-loading">Vui lòng chờ...</p>`;
+        setTimeout(() => {
+            window.location.reload();
+        }, 1000);
+    });
 }
 
 function updateLogger() {
-  loggerContainer.innerHTML = logger[count];
-  const handelClick = () => {
-    startCount = true;
-  };
-  welcomeContainer.addEventListener("click", handelClick);
-  if (startCount) {
-    welcomeContainer.removeEventListener("click", handelClick);
-    count++;
-  }
-  if (count >= logger.length) {
-    clearInterval(clearID);
-    welcomeContainer.classList.add("hidden");
-    start = true;
-    startGame();
-  }
+    loggerContainer.innerHTML = logger[count];
+    const handelClick = () => {
+        startCount = true;
+    };
+    welcomeContainer.addEventListener("click", handelClick);
+    if (startCount) {
+        welcomeContainer.removeEventListener("click", handelClick);
+        count++;
+    }
+    if (count >= logger.length) {
+        clearInterval(clearID);
+        welcomeContainer.classList.add("hidden");
+        start = true;
+        startGame();
+    }
 }
 
 //board
@@ -160,10 +167,10 @@ let birdY = boardHeight / 2;
 let birdImg;
 
 let bird = {
-  x: birdX,
-  y: birdY,
-  width: birdWidth,
-  height: birdHeight,
+    x: birdX,
+    y: birdY,
+    width: birdWidth,
+    height: birdHeight,
 };
 
 //pipes
@@ -186,11 +193,11 @@ let giftCount = 0;
 let giftPass = false;
 
 let gift = {
-  x: giftX,
-  y: giftY,
-  width: giftWidth,
-  height: giftHeight,
-  isPassed: false,
+    x: giftX,
+    y: giftY,
+    width: giftWidth,
+    height: giftHeight,
+    isPassed: false,
 };
 
 // Mark
@@ -204,160 +211,160 @@ let timer = 2500;
 let gameOver = false;
 let score = 0;
 
-window.onload = function () {
-  // Load Letter
-  letterImg = document.querySelector("#letter-img");
-  letterDownload = document.querySelector("#download-btn");
-  let letterImage = new Image();
-  letterImage.src = `../assets/img/${letterArray[id]}`;
-  letterDownload.setAttribute("href", `../assets/img/${letterArray[id]}`);
-  letterImage.onload = () => {
-    checkLoaded();
-    letterImg.src = letterImage.src;
-  };
+window.onload = function() {
+    // Load Letter
+    letterImg = document.querySelector("#letter-img");
+    letterDownload = document.querySelector("#download-btn");
+    let letterImage = new Image();
+    letterImage.src = `../assets/img/${letterArray[id]}`;
+    letterDownload.setAttribute("href", `../assets/img/${letterArray[id]}`);
+    letterImage.onload = () => {
+        checkLoaded();
+        letterImg.src = letterImage.src;
+    };
 
-  board = document.getElementById("board");
-  board.height = boardHeight;
-  board.width = boardWidth;
-  context = board.getContext("2d");
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+    context = board.getContext("2d");
 
-  //Load Image
-  birdImg = new Image();
-  birdImg.src = "./assets/img/bird.png";
-  birdImg.onload = function () {
-    checkLoaded();
-    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
-  };
+    //Load Image
+    birdImg = new Image();
+    birdImg.src = "./assets/img/bird.png";
+    birdImg.onload = function() {
+        checkLoaded();
+        context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+    };
 
-  // Load Gift
-  giftImg = new Image();
-  giftImg.src = "./assets/img/gift.png";
+    // Load Gift
+    giftImg = new Image();
+    giftImg.src = "./assets/img/gift.png";
 
-  // Load Pipe
-  topPipeImg = new Image();
-  topPipeImg.src = "./assets/img/pipe.png";
+    // Load Pipe
+    topPipeImg = new Image();
+    topPipeImg.src = "./assets/img/pipe.png";
 
-  bottomPipeImg = new Image();
-  bottomPipeImg.src = "./assets/img/pipe.png";
+    bottomPipeImg = new Image();
+    bottomPipeImg.src = "./assets/img/pipe.png";
 
-  // Ready
-  loggerContainer = document.querySelector("#welcome-log");
-  welcomeContainer = document.querySelector(".welcome");
+    // Ready
+    loggerContainer = document.querySelector("#welcome-log");
+    welcomeContainer = document.querySelector(".welcome");
 
-  // Mark
-  markContainer = document.querySelector("#mark");
+    // Mark
+    markContainer = document.querySelector("#mark");
 };
 
 function startGame() {
-  requestAnimationFrame(update);
-  setInterval(placePipes, timer);
-  board.addEventListener("click", (e) => {
-    e.preventDefault();
-    moveBird();
-  });
+    requestAnimationFrame(update);
+    setInterval(placePipes, timer);
+    board.addEventListener("click", (e) => {
+        e.preventDefault();
+        moveBird();
+    });
 }
 
 function update() {
-  requestAnimationFrame(update);
-  if (gameOver) {
-    letterAction();
-    return;
-  }
-  if (start) {
-    // Kiểm tra nếu start đã bằng true
-    context.clearRect(0, 0, board.width, board.height);
-  }
-
-  // Gift
-  // Đã đụng trúng
-  gift.x -= 2;
-  if (gift.isPassed === true) {
-    gift.x += 4;
-    gift.y -= 4;
-  } else if (gift.isPassed === false && gift.x < 0) {
-    gift.x = boardWidth * 1.5;
-    gift.isPassed = false;
-  }
-  if (detectCollision(bird, gift)) {
-    gift.isPassed = true;
-    setTimeout(() => {
-      gameOver = true;
-    }, 1000);
-  }
-  context.drawImage(giftImg, gift.x, gift.y, gift.width, gift.height);
-
-  //pipes
-  for (let i = 0; i < pipeArray.length; i++) {
-    let pipe = pipeArray[i];
-    pipe.x += velocityX;
-    context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
-
-    if (!pipe.passed && bird.x > pipe.x + pipe.width) {
-      score += 0.5;
-      pipe.passed = true;
+    requestAnimationFrame(update);
+    if (gameOver) {
+        letterAction();
+        return;
+    }
+    if (start) {
+        // Kiểm tra nếu start đã bằng true
+        context.clearRect(0, 0, board.width, board.height);
     }
 
-    if (detectCollision(bird, pipe)) {
-      gameOver = true;
+    // Gift
+    // Đã đụng trúng
+    gift.x -= 2;
+    if (gift.isPassed === true) {
+        gift.x += 4;
+        gift.y -= 4;
+    } else if (gift.isPassed === false && gift.x < 0) {
+        gift.x = boardWidth * 1.5;
+        gift.isPassed = false;
     }
-  }
+    if (detectCollision(bird, gift)) {
+        gift.isPassed = true;
+        setTimeout(() => {
+            gameOver = true;
+        }, 1000);
+    }
+    context.drawImage(giftImg, gift.x, gift.y, gift.width, gift.height);
 
-  //clear pipes
-  while (pipeArray.length > 0 && pipeArray[0].x < -pipeWidth) {
-    pipeArray.shift();
-  }
+    //pipes
+    for (let i = 0; i < pipeArray.length; i++) {
+        let pipe = pipeArray[i];
+        pipe.x += velocityX;
+        context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
 
-  //bird
-  velocityY += gravity;
-  bird.y = Math.max(bird.y + velocityY, 0);
-  context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+        if (!pipe.passed && bird.x > pipe.x + pipe.width) {
+            score += 0.5;
+            pipe.passed = true;
+        }
 
-  if (bird.y >= board.height) {
-    gameOver = true;
-  }
+        if (detectCollision(bird, pipe)) {
+            gameOver = true;
+        }
+    }
 
-  //score
-  markContainer.innerHTML = score;
+    //clear pipes
+    while (pipeArray.length > 0 && pipeArray[0].x < -pipeWidth) {
+        pipeArray.shift();
+    }
+
+    //bird
+    velocityY += gravity;
+    bird.y = Math.max(bird.y + velocityY, 0);
+    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+
+    if (bird.y >= board.height) {
+        gameOver = true;
+    }
+
+    //score
+    markContainer.innerHTML = score;
 }
 
 function placePipes() {
-  if (gameOver) {
-    return;
-  }
+    if (gameOver) {
+        return;
+    }
 
-  let randomPipeY = pipeY - pipeHeight / 4 - Math.random() * (pipeHeight / 2);
-  let openingSpace = board.height / 2.7;
+    let randomPipeY = pipeY - pipeHeight / 4 - Math.random() * (pipeHeight / 2);
+    let openingSpace = board.height / 2.7;
 
-  let topPipe = {
-    img: topPipeImg,
-    x: pipeX,
-    y: randomPipeY,
-    width: pipeWidth,
-    height: pipeHeight,
-    passed: false,
-  };
-  pipeArray.push(topPipe);
+    let topPipe = {
+        img: topPipeImg,
+        x: pipeX,
+        y: randomPipeY,
+        width: pipeWidth,
+        height: pipeHeight,
+        passed: false,
+    };
+    pipeArray.push(topPipe);
 
-  let bottomPipe = {
-    img: bottomPipeImg,
-    x: pipeX,
-    y: randomPipeY + pipeHeight + openingSpace,
-    width: pipeWidth,
-    height: pipeHeight,
-    passed: false,
-  };
-  pipeArray.push(bottomPipe);
+    let bottomPipe = {
+        img: bottomPipeImg,
+        x: pipeX,
+        y: randomPipeY + pipeHeight + openingSpace,
+        width: pipeWidth,
+        height: pipeHeight,
+        passed: false,
+    };
+    pipeArray.push(bottomPipe);
 }
 
 function moveBird() {
-  velocityY = -6;
+    velocityY = -6;
 }
 
 function detectCollision(a, b) {
-  return (
-    a.x < b.x + b.width &&
-    a.x + a.width > b.x &&
-    a.y < b.y + b.height &&
-    a.y + a.height > b.y
-  );
+    return (
+        a.x < b.x + b.width &&
+        a.x + a.width > b.x &&
+        a.y < b.y + b.height &&
+        a.y + a.height > b.y
+    );
 }
